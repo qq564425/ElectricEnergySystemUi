3dfc76a3fefab4871ae72dd909b4e89bb29f82cd
wangweiweihdnav <1585347327@qq.com>
2019-04-16 21:10:48 +0800

    20190416

--- src/pages/Test.vue
+++ src/pages/Test.vue
@@ -1,90 +1,30 @@
 <template>
-  <a-form :form="form">
-    <a-form-item
-      :label-col="formItemLayout.labelCol"
-      :wrapper-col="formItemLayout.wrapperCol"
-      label="Name"
-    >
-      <a-input
-        v-decorator="[
-          'username',
-          {rules: [{ required: true, message: 'Please input your name' }]}
-        ]"
-        placeholder="Please input your name"
-      />
-    </a-form-item>
-    <a-form-item
-      :label-col="formItemLayout.labelCol"
-      :wrapper-col="formItemLayout.wrapperCol"
-      label="Nickname"
-    >
-      <a-input
-        v-decorator="[
-          'nickname',
-          {rules: [{ required: checkNick, message: 'Please input your nickname' }]}
-        ]"
-        placeholder="Please input your nickname"
-      />
-    </a-form-item>
-    <a-form-item
-      :label-col="formTailLayout.labelCol"
-      :wrapper-col="formTailLayout.wrapperCol"
-    >
-      <a-checkbox
-        :checked="checkNick"
-        @change="handleChange"
-      >
-        Nickname is required
-      </a-checkbox>
-    </a-form-item>
-    <a-form-item
-      :label-col="formTailLayout.labelCol"
-      :wrapper-col="formTailLayout.wrapperCol"
-    >
-      <a-button
-        type="primary"
-        @click="check"
-      >
-        Check
-      </a-button>
-    </a-form-item>
-  </a-form>
+  <a-popover
+    title="Title"
+    trigger="click"
+    v-model="visible"
+  >  
+    <template slot="content">
+        <p>Content</p>
+        <p>Content</p>
+    </template>
+    <a @click="hide" slot="content">Close</a>
+    <a-button type="primary">Click me</a-button>
+  </a-popover>
 </template>
 
 <script>
-const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8 },
-};
-const formTailLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8, offset: 4 },
-};
 export default {
   data () {
     return {
-      checkNick: false,
-      formItemLayout,
-      formTailLayout,
-      form: this.$form.createForm(this),
-    };
+      visible: false,
+    }
   },
   methods: {
-    check  () {
-      this.form.validateFields(
-        (err) => {
-          if (!err) {
-            console.info('success');
-          }
-        },
-      );
-    },
-    handleChange  (e) {
-      this.checkNick = e.target.checked;
-      this.$nextTick(() => {
-        this.form.validateFields(['nickname'], { force: true });
-      });
+    hide () {
+      console.log(111)
+      this.visible = false
     },
   },
-};
+}
 </script>
